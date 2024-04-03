975065a0233806b381d2afc0e3e766f6d97aac26
TayfurSafakGencay <safak.gencay@gmail.com>
2024-04-03 11:28:25 +0300

    New: Level Select Panel added.

---
+++ Assets/Scripts/Panel/LevelSelectMenu/LevelSelectPanel.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Panel.LevelSelectMenu
+{
+    public class LevelSelectPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private LevelSelectPanelItem _levelSelectPanelItem;
+
+        [SerializeField]
+        private Transform _itemContainer;
+
+        public int LevelCount;
+
+        private void Start()
+        {
+            for (int i = 1; i < LevelCount + 1; i++)
+            {
+                LevelSelectPanelItem item = Instantiate(_levelSelectPanelItem, transform.position, quaternion.identity, _itemContainer);
+                item.Init(i, PlayerPrefs.GetInt("Level " + i, 0), false);
+            }
+        }
+    }
+}
