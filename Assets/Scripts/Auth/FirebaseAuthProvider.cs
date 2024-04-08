f61155e5eaac1bd88a963d6e6ff696ae9de8a0aa
Tamer Erdoğan <tamer.erdogaan@gmail.com>
2024-04-08 14:07:31 +0300

    FirebaseAuth and FirebaseFirestore systems added.

---
+++ Assets/Scripts/Auth/FirebaseAuthProvider.cs
@@ -0,0 +1,73 @@
+//Author: Tamer Erdoğan
+
+using System;
+using System.Collections;
+using Firebase.Auth;
+using UnityEngine;
+
+public class FirebaseAuthProvider : AbstractAuthProvider
+{
+    private FirebaseAuth _auth;
+
+    public override void Init()
+    {
+        _auth = FirebaseAuth.DefaultInstance;
+    }
+
+    public override string GetCurrentUserId()
+    {
+        return _auth.CurrentUser.UserId;
+    }
+
+    public override void Login(
+        string email,
+        string password,
+        Action onLoginSuccess = null,
+        Action<string> onLoginFailed = null
+    )
+    {
+        StartCoroutine(LoginRequest(email, password, onLoginSuccess, onLoginFailed));
+    }
+
+    IEnumerator LoginRequest(
+        string email,
+        string password,
+        Action onLoginSuccess = null,
+        Action<string> onLoginFailed = null
+    )
+    {
+        var loginRequestTask = _auth.SignInWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(() => loginRequestTask.IsCompleted);
+
+        if (loginRequestTask.Exception == null)
+            onLoginSuccess?.Invoke();
+        else
+            onLoginFailed?.Invoke("An error occurred while login.");
+    }
+
+    public override void Register(
+        string email,
+        string password,
+        Action<string> onRegisterSuccess = null,
+        Action<string> onRegisterFailed = null
+    )
+    {
+        StartCoroutine(RegisterRequest(email, password, onRegisterSuccess, onRegisterFailed));
+    }
+
+    IEnumerator RegisterRequest(
+        string email,
+        string password,
+        Action<string> onRegisterSuccess = null,
+        Action<string> onRegisterFailed = null
+    )
+    {
+        var registerRequestTask = _auth.CreateUserWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(() => registerRequestTask.IsCompleted);
+
+        if (registerRequestTask.Exception == null)
+            onRegisterSuccess?.Invoke(registerRequestTask.Result.User.UserId);
+        else
+            onRegisterFailed?.Invoke(registerRequestTask.Exception.InnerException.Message);
+    }
+}
---
+++ Assets/Scripts/FirestoreData/UserFD.cs
@@ -0,0 +1,16 @@
+//Author: Tamer Erdoğan
+
+using Firebase.Firestore;
+
+[FirestoreData]
+public class UserFD
+{
+    [FirestoreProperty]
+    public string id { get; set; }
+
+    [FirestoreProperty]
+    public int score { get; set; } = 0;
+
+    [FirestoreProperty]
+    public int level { get; set; } = 0;
+}
